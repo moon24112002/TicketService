7eb2c1b58dfb2dccea5860af1b5b1b574155c3cb
MamoonAkhter <akhtermamoon1@gmail.com>
2025-05-28 23:38:38 +0530

    added cors

--- src/main/java/com/ticketingsystem/TicketSystemApplication.java
+++ src/main/java/com/ticketingsystem/TicketSystemApplication.java
@@ -5,9 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TicketSystemApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000") // Frontend origin
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
--- src/main/java/com/ticketingsystem/utility/TicketDetails.java
+++ src/main/java/com/ticketingsystem/utility/TicketDetails.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public class TicketDetails {
     public static List<User> list = List.of(new User(UUID.randomUUID().toString(),"Rachit","xyz@gmail.com"),
             new User(UUID.randomUUID().toString(),"Rachit","xyz@gmail.com"),
-    new User(UUID.randomUUID().toString(),"Aurojit","xyz@gmail.com"),
+            new User(UUID.randomUUID().toString(),"Aurojit","xyz@gmail.com"),
             new User(UUID.randomUUID().toString(),"Anurag","xyz@gmail.com"),
             new User(UUID.randomUUID().toString(),"Abhisekh","xyz@gmail.com"),
             new User(UUID.randomUUID().toString(),"Satish","xyz@gmail.com"),
