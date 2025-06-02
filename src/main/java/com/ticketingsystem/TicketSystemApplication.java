ca791ce01b7097b1433811653d7850fd007b86da
MamoonAkhter <akhtermamoon1@gmail.com>
2025-06-03 01:07:48 +0530

    added cors

--- src/main/java/com/ticketingsystem/TicketSystemApplication.java
+++ src/main/java/com/ticketingsystem/TicketSystemApplication.java
@@ -8,18 +8,4 @@ public class TicketSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TicketSystemApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000") // Frontend origin
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
-	}
 }
